a04104f5e7d3b1e2347e60bb7900e9f9cc955875
Ryan Jennings <rybertjen@gmail.com>
2025-07-31 17:42:13 -0700

    feat(bluebell, scan): update app config, downloads, and model analyzers/generators

--- composeApp/src/commonMain/kotlin/com/micrantha/eyespie/platform/scan/generator/ImageStyler.kt
+++ composeApp/src/commonMain/kotlin/com/micrantha/eyespie/platform/scan/generator/ImageStyler.kt
@@ -1,9 +1,8 @@
 package com.micrantha.eyespie.platform.scan.generator
 
-import com.micrantha.eyespie.domain.entities.Clues
 import com.micrantha.eyespie.platform.scan.CameraImage
 import com.micrantha.eyespie.platform.scan.components.ImageGenerator
 
-expect class ImageCluesGenerator : ImageGenerator<Clues> {
-    override fun generate(from: Clues): Result<CameraImage>
+expect class ImageStyler : ImageGenerator<CameraImage> {
+    override suspend fun generate(from: CameraImage): Result<CameraImage>
 }
