5949fe6eefef7a641dc74fd36974c73908cb7aac
RJ <rybertjen@gmail.com>
2026-07-19 01:57:45 -0700

    feat(onboarding): add capability authorization model

---
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/onboarding/entities/CapabilityAuthorization.kt
@@ -0,0 +1,26 @@
+package com.micrantha.eyespie.features.onboarding.entities
+
+enum class OnboardingCapability {
+    CameraScanning,
+    Notifications,
+}
+
+sealed interface CapabilityAuthorization {
+    data object Unsupported : CapabilityAuthorization
+    data object NotRequired : CapabilityAuthorization
+    data object NotRequested : CapabilityAuthorization
+    data object Granted : CapabilityAuthorization
+    data object Denied : CapabilityAuthorization
+    data object Restricted : CapabilityAuthorization
+    data object SettingsRequired : CapabilityAuthorization
+}
+
+data class CapabilityUiState(
+    val capability: OnboardingCapability,
+    val title: String,
+    val rationale: String,
+    val deniedImpact: String,
+    val privacySummary: String,
+    val authorization: CapabilityAuthorization,
+    val canRequestDuringOnboarding: Boolean,
+)
