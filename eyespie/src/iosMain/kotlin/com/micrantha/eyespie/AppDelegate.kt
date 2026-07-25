1f38c01a30b1c1d85606721f9c560c4a4c684259
RJ <rybertjen@gmail.com>
2026-07-24 22:48:14 -0700

    feat(ios): expose model delivery capability seam

--- eyespie/src/iosMain/kotlin/com/micrantha/eyespie/AppDelegate.kt
+++ eyespie/src/iosMain/kotlin/com/micrantha/eyespie/AppDelegate.kt
@@ -1,8 +1,13 @@
 package com.micrantha.eyespie
 
 import com.micrantha.bluebell.platform.NetworkMonitor
+import com.micrantha.eyespie.model.DefaultIosModelDeliveryCapabilities
+import com.micrantha.eyespie.model.IosModelDeliveryCapabilities
 
 class AppDelegate(
     val networkMonitor: NetworkMonitor,
     val packageId: String,
-)
+) {
+    var modelDeliveryCapabilities: IosModelDeliveryCapabilities =
+        DefaultIosModelDeliveryCapabilities
+}
