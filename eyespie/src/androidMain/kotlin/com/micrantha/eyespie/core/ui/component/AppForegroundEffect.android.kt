d216fec9772580c51cf24b3b051238c5b26f1100
RJ <rybertjen@gmail.com>
2026-07-25 01:08:38 -0700

    Use existing Compose lifecycle owner

--- eyespie/src/androidMain/kotlin/com/micrantha/eyespie/core/ui/component/AppForegroundEffect.android.kt
+++ eyespie/src/androidMain/kotlin/com/micrantha/eyespie/core/ui/component/AppForegroundEffect.android.kt
@@ -4,10 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.compose.LocalLifecycleOwner
 
+@Suppress("DEPRECATION")
 @Composable
 actual fun AppForegroundEffect(onForeground: () -> Unit) {
     val currentOnForeground by rememberUpdatedState(onForeground)
