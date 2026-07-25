ffdff8dfbc4a8b8bf3b522a5b00fb2da8b3d7b6a
RJ <rybertjen@gmail.com>
2026-07-25 14:45:02 -0700

    Track scan camera authorization state

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanState.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanState.kt
@@ -2,10 +2,14 @@ package com.micrantha.eyespie.features.scan.entities
 
 import androidx.compose.runtime.Stable
 import com.micrantha.eyespie.domain.entities.Location
+import com.micrantha.eyespie.features.onboarding.entities.CapabilityAuthorization
 
 @Stable
 data class ScanState(
     val enabled: Boolean = true,
     val busy: Boolean = false,
     val location: Location? = null,
+    val cameraAuthorization: CapabilityAuthorization = CapabilityAuthorization.NotRequested,
+    val cameraAuthorizationLoaded: Boolean = false,
+    val cameraRequestInFlight: Boolean = false,
 )
