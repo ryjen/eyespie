7c2ecc00c06cbf36e23fb2329062b1f7fe66ced2
RJ <rybertjen@gmail.com>
2026-07-25 01:23:55 -0700

    Normalize iOS notification authorization

--- eyespie/src/iosMain/kotlin/com/micrantha/eyespie/features/onboarding/data/CapabilityPermissionGateway.ios.kt
+++ eyespie/src/iosMain/kotlin/com/micrantha/eyespie/features/onboarding/data/CapabilityPermissionGateway.ios.kt
@@ -1,7 +1,9 @@
 package com.micrantha.eyespie.features.onboarding.data
 
 import com.micrantha.eyespie.features.onboarding.entities.CapabilityAuthorization
+import dev.icerock.moko.permissions.Permission
 import dev.icerock.moko.permissions.PermissionsController
+import dev.icerock.moko.permissions.notifications.REMOTE_NOTIFICATION
 import platform.AVFoundation.AVAuthorizationStatusAuthorized
 import platform.AVFoundation.AVAuthorizationStatusDenied
 import platform.AVFoundation.AVAuthorizationStatusNotDetermined
@@ -23,3 +25,10 @@ internal actual suspend fun currentCameraAuthorization(
     AVAuthorizationStatusRestricted -> CapabilityAuthorization.Restricted
     else -> CapabilityAuthorization.Unsupported
 }
+
+internal actual suspend fun currentNotificationAuthorization(
+    permissionsController: PermissionsController,
+    previous: CapabilityAuthorization,
+): CapabilityAuthorization = permissionsController
+    .getPermissionState(Permission.REMOTE_NOTIFICATION)
+    .toCapabilityAuthorization(previous)
