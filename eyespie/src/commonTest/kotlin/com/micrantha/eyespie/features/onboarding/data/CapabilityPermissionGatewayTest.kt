07ad7814931cecc87d7fdecc0789be9fd1ab8aef
RJ <rybertjen@gmail.com>
2026-07-25 16:16:30 -0700

    fix(test): keep platform reconciliation out of common tests

--- eyespie/src/commonTest/kotlin/com/micrantha/eyespie/features/onboarding/data/CapabilityPermissionGatewayTest.kt
+++ eyespie/src/commonTest/kotlin/com/micrantha/eyespie/features/onboarding/data/CapabilityPermissionGatewayTest.kt
@@ -1,11 +1,7 @@
 package com.micrantha.eyespie.features.onboarding.data
 
 import com.micrantha.eyespie.features.onboarding.entities.CapabilityAuthorization
-import com.micrantha.eyespie.features.onboarding.entities.OnboardingCapability
-import dev.icerock.moko.permissions.Permission
 import dev.icerock.moko.permissions.PermissionState
-import dev.icerock.moko.permissions.test.PermissionsControllerMock
-import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -46,30 +42,5 @@ class CapabilityPermissionGatewayTest {
                 CapabilityAuthorization.SettingsRequired,
             ),
         )
-    }
-
-    @Test
-    fun `camera request returns reconciled authorization`() = runTest {
-        var requestCount = 0
-        val permissions = object : PermissionsControllerMock() {
-            override suspend fun providePermission(permission: Permission) {
-                requestCount += 1
-            }
-
-            override suspend fun isPermissionGranted(permission: Permission) = true
-
-            override suspend fun getPermissionState(permission: Permission) = PermissionState.Granted
-
-            override fun openAppSettings() = Unit
-        }
-        val gateway = MokoCapabilityPermissionGateway(permissions)
-
-        val result = gateway.requestAuthorization(
-            OnboardingCapability.CameraScanning,
-            CapabilityAuthorization.NotRequested,
-        )
-
-        assertEquals(CapabilityAuthorization.Granted, result)
-        assertEquals(1, requestCount)
     }
 }
