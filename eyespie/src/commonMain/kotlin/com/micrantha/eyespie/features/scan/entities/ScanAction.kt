6c436e5522d78f93f1294109e240df2d5aef18fe
RJ <rybertjen@gmail.com>
2026-07-25 14:44:54 -0700

    Add contextual camera authorization actions

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanAction.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanAction.kt
@@ -1,12 +1,28 @@
 package com.micrantha.eyespie.features.scan.entities
 
+import com.micrantha.eyespie.features.onboarding.entities.CapabilityAuthorization
 import com.micrantha.eyespie.platform.scan.CameraImage
 import okio.Path
 
 sealed interface ScanAction {
+    data object RefreshCameraAuthorization : ScanAction
+    data object RequestCameraAuthorization : ScanAction
+    data object CameraAuthorizationRequestStarted : ScanAction
+    data class CameraAuthorizationLoaded(
+        val previous: CapabilityAuthorization,
+        val authorization: CapabilityAuthorization,
+    ) : ScanAction
+
+    data class CameraAuthorizationRequestResolved(
+        val authorization: CapabilityAuthorization,
+    ) : ScanAction
+
+    data object CameraAuthorizationRequestFailed : ScanAction
+    data object OpenCameraSettings : ScanAction
+
     data class SaveScan(
         val image: CameraImage,
-        val path: Path
+        val path: Path,
     ) : ScanAction
 
     data object ScanError : ScanAction
