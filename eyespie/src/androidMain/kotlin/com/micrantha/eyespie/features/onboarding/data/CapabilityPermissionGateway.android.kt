d749af4ea28ad067a5493b59f1f649dc29a7e493
RJ <rybertjen@gmail.com>
2026-07-25 01:23:33 -0700

    Hide notification permission on pre-Android 13 devices

--- eyespie/src/androidMain/kotlin/com/micrantha/eyespie/features/onboarding/data/CapabilityPermissionGateway.android.kt
+++ eyespie/src/androidMain/kotlin/com/micrantha/eyespie/features/onboarding/data/CapabilityPermissionGateway.android.kt
@@ -1,9 +1,11 @@
 package com.micrantha.eyespie.features.onboarding.data
 
+import android.os.Build
 import com.micrantha.eyespie.features.onboarding.entities.CapabilityAuthorization
 import dev.icerock.moko.permissions.Permission
 import dev.icerock.moko.permissions.PermissionsController
 import dev.icerock.moko.permissions.camera.CAMERA
+import dev.icerock.moko.permissions.notifications.REMOTE_NOTIFICATION
 
 internal actual suspend fun currentCameraAuthorization(
     permissionsController: PermissionsController,
@@ -11,3 +13,14 @@ internal actual suspend fun currentCameraAuthorization(
 ): CapabilityAuthorization = permissionsController
     .getPermissionState(Permission.CAMERA)
     .toCapabilityAuthorization(previous)
+
+internal actual suspend fun currentNotificationAuthorization(
+    permissionsController: PermissionsController,
+    previous: CapabilityAuthorization,
+): CapabilityAuthorization = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+    CapabilityAuthorization.NotRequired
+} else {
+    permissionsController
+        .getPermissionState(Permission.REMOTE_NOTIFICATION)
+        .toCapabilityAuthorization(previous)
+}
