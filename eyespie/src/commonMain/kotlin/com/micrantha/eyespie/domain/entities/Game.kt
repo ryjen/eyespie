613cd09d78079282d7038797c4bab48567e536df
RJ <rybertjen@gmail.com>
2026-07-26 15:54:22 -0700

    fix(ios): hide game models from Objective-C export

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/domain/entities/Game.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/domain/entities/Game.kt
@@ -1,12 +1,15 @@
 package com.micrantha.eyespie.domain.entities
 
 import com.micrantha.eyespie.features.players.domain.entities.Player
+import kotlin.experimental.ExperimentalObjCRefinement
+import kotlin.native.HiddenFromObjC
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
-@OptIn(ExperimentalTime::class)
-data class Game internal constructor(
+@OptIn(ExperimentalTime::class, ExperimentalObjCRefinement::class)
+@HiddenFromObjC
+data class Game(
     override val id: String,
     override val createdAt: Instant,
     val name: String,
@@ -22,7 +25,7 @@ data class Game internal constructor(
         val thing: IntRange
     )
 
-    data class Listing internal constructor(
+    data class Listing(
         override val id: String,
         val nodeId: String,
         override val createdAt: Instant,
