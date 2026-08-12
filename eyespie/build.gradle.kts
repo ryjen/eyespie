2f788732db16c38a482dce16e1519a9fe050d2f3
RJ <rybertjen@gmail.com>
2026-08-12 12:50:41 -0700

    fix(ios): restore Ktor serialization alias

--- eyespie/build.gradle.kts
+++ eyespie/build.gradle.kts
@@ -153,7 +153,7 @@ kotlin {
             implementation(libs.voyager.kodein)
 
             implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.serialization.kotlinx)
+            implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.ktor.client.logging)
             implementation(libs.ktor.client.auth)
             implementation(libs.ktor.client.cio)
