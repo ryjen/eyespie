00b17af2f464d8de46e0aa8ca2b32dc9044eff57
RJ <rybertjen@gmail.com>
2026-08-11 23:36:25 -0700

    test(ios): use TestScope background scope member

--- eyespie/src/appleTest/kotlin/com/micrantha/eyespie/model/IosModelAssetRepositoryTest.kt
+++ eyespie/src/appleTest/kotlin/com/micrantha/eyespie/model/IosModelAssetRepositoryTest.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.test.backgroundScope
 import kotlinx.coroutines.test.runTest
 import org.kodein.di.DI
 import org.kodein.di.direct
@@ -113,7 +112,7 @@ class IosModelAssetRepositoryTest {
                 recoverable = false,
                 diagnosticCode = "model_delivery_schedule_failed",
             ),
-            repository.observe().first(),
+            repository.observe().first { it is ModelAssetState.Failed },
         )
         repository.close()
     }
