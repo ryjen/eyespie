0069ea2bac6bc5be407b56b24d0625fc4f973eda
RJ <rybertjen@gmail.com>
2026-08-13 20:14:28 -0700

    test(scan): keep fake router minimal

--- eyespie/src/commonTest/kotlin/com/micrantha/eyespie/core/ui/FakeScreenContext.kt
+++ eyespie/src/commonTest/kotlin/com/micrantha/eyespie/core/ui/FakeScreenContext.kt
@@ -16,10 +16,8 @@ class FakeRouter : Router {
     var lastNavigatedTo: Screen? = null
     var lastOptions: Options? = null
     var navigateBackCalled = false
-    var navigateFailure: Throwable? = null
 
     override fun <T : Screen> navigate(screen: T, options: Options) {
-        navigateFailure?.let { throw it }
         lastNavigatedTo = screen
         lastOptions = options
     }
