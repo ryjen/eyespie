de048354fe837d7f52e7e1575451266ec8fced18
RJ <rybertjen@gmail.com>
2026-08-14 02:59:10 -0700

    feat(scan): retain generated clue provenance in edit state

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanEditState.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanEditState.kt
@@ -1,7 +1,7 @@
 package com.micrantha.eyespie.features.scan.entities
 
 import com.micrantha.bluebell.domain.StateMap
-import com.micrantha.eyespie.domain.entities.AiProof
+import com.micrantha.eyespie.domain.ai.GeneratedClues
 import com.micrantha.eyespie.domain.entities.Embedding
 import com.micrantha.eyespie.domain.entities.Location
 import com.micrantha.eyespie.platform.scan.CameraImage
@@ -9,7 +9,7 @@ import okio.Path
 
 data class ScanEditState(
     val image: CameraImage? = null,
-    val clues: AiProof? = null,
+    val clues: GeneratedClues? = null,
     val selected: StateMap<Int, ScanClue>? = null,
     val embedding: Embedding? = null,
     val name: String? = null,
