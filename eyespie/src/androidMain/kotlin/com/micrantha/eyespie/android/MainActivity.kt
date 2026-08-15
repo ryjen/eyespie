2ca3fad2dd5a1bc03eab018b8575293625616f1a
RJ <rybertjen@gmail.com>
2026-08-15 13:25:18 -0700

    feat(sharing): layer Android bundle controls over app

--- eyespie/src/androidMain/kotlin/com/micrantha/eyespie/android/MainActivity.kt
+++ eyespie/src/androidMain/kotlin/com/micrantha/eyespie/android/MainActivity.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.micrantha.eyespie.App
 import com.micrantha.eyespie.AppUnavailable
 import com.micrantha.eyespie.game.createAndroidEyespieRuntime
+import com.micrantha.eyespie.sharing.AndroidBundleActions
 import com.micrantha.eyespie.sharing.rememberAndroidGameDocumentTransfer
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +36,20 @@ class MainActivity : ComponentActivity() {
                 AppUnavailable()
             } else {
                 val documentTransfer = rememberAndroidGameDocumentTransfer()
-                App(runtime, documentTransfer)
+                var contentVersion by remember { mutableIntStateOf(0) }
+                Box {
+                    key(contentVersion) {
+                        App(runtime)
+                    }
+                    AndroidBundleActions(
+                        runtime = runtime,
+                        transfer = documentTransfer,
+                        onImported = { contentVersion += 1 },
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(16.dp),
+                    )
+                }
             }
         }
     }
