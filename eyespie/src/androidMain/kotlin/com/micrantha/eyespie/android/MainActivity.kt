7776a9ccac7ab0aad1ac378c46bfdd7fc2aca542
RJ <rybertjen@gmail.com>
2026-08-15 04:40:44 -0700

    feat(game): bootstrap Android offline runtime

--- eyespie/src/androidMain/kotlin/com/micrantha/eyespie/android/MainActivity.kt
+++ eyespie/src/androidMain/kotlin/com/micrantha/eyespie/android/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.remember
 import com.micrantha.eyespie.App
+import com.micrantha.eyespie.AppUnavailable
+import com.micrantha.eyespie.game.createAndroidEyespieRuntime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { App() }
+        setContent {
+            val runtime = remember { runCatching { createAndroidEyespieRuntime(this) }.getOrNull() }
+            if (runtime == null) {
+                AppUnavailable()
+            } else {
+                App(runtime)
+            }
+        }
     }
 }
