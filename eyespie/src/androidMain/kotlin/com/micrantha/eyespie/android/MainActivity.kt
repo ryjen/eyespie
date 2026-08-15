1ec96981ede15724f57feda94970c9a191425c84
RJ <rybertjen@gmail.com>
2026-08-15 04:50:54 -0700

    feat(game): inject Android offline runtime

--- eyespie/src/androidMain/kotlin/com/micrantha/eyespie/android/MainActivity.kt
+++ eyespie/src/androidMain/kotlin/com/micrantha/eyespie/android/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.micrantha.eyespie.App
+import com.micrantha.eyespie.game.createAndroidOfflineRuntime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { App() }
+        val runtime = createAndroidOfflineRuntime(applicationContext)
+        setContent { App(runtime) }
     }
 }
