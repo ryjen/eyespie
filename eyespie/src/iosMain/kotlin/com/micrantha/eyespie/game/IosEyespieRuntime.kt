e456f86b20ddb6bb92f0ea6ab010f67f8b29f9c7
RJ <rybertjen@gmail.com>
2026-08-15 13:24:16 -0700

    feat(sharing): wire iOS bundle service

--- eyespie/src/iosMain/kotlin/com/micrantha/eyespie/game/IosEyespieRuntime.kt
+++ eyespie/src/iosMain/kotlin/com/micrantha/eyespie/game/IosEyespieRuntime.kt
@@ -3,21 +3,32 @@ package com.micrantha.eyespie.game
 import com.micrantha.eyespie.core.GameId
 import com.micrantha.eyespie.core.ThingId
 import com.micrantha.eyespie.identity.LocalPlayerIdentityRepository
+import com.micrantha.eyespie.identity.PlatformSigningIdentity
 import com.micrantha.eyespie.imaging.MediaPipeImageEmbeddingGenerator
 import com.micrantha.eyespie.persistence.IosEyespieDatabaseFactory
 import com.micrantha.eyespie.persistence.SqlGameRepository
 import com.micrantha.eyespie.persistence.SqlThingProgressRepository
+import com.micrantha.eyespie.sharing.GameBundleService
 import platform.Foundation.NSUUID
 
 fun createIosEyespieRuntime(): EyespieRuntime {
     val database = IosEyespieDatabaseFactory().create()
+    val signingIdentity = PlatformSigningIdentity()
+    val identityRepository = LocalPlayerIdentityRepository(signingIdentity)
+    val gameRepository = SqlGameRepository(database)
+
     return EyespieRuntime(
-        LocalGameLoop(
-            identityRepository = LocalPlayerIdentityRepository(),
-            gameRepository = SqlGameRepository(database),
+        gameLoop = LocalGameLoop(
+            identityRepository = identityRepository,
+            gameRepository = gameRepository,
             progressRepository = SqlThingProgressRepository(database),
             embeddingGenerator = MediaPipeImageEmbeddingGenerator(),
             idGenerator = IosLocalGameIdGenerator(),
+        ),
+        bundleService = GameBundleService(
+            identityRepository = identityRepository,
+            signingIdentity = signingIdentity,
+            gameRepository = gameRepository,
         ),
     )
 }
