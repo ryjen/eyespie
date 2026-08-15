e5b1af886f903ca6d53793abd5fc98b19dd9e486
RJ <rybertjen@gmail.com>
2026-08-14 19:28:47 -0700

    refactor(game): decouple safe details from generated GraphQL types

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/game/data/model/GameRemoteDetails.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/game/data/model/GameRemoteDetails.kt
@@ -1,12 +1,29 @@
 package com.micrantha.eyespie.features.game.data.model
 
-import com.micrantha.eyespie.graphql.GameNodeQuery
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 internal data class GameRemoteDetails(
-    val node: GameNodeQuery.GameNode,
+    val id: String,
+    val name: String,
+    val createdAt: String,
+    val expiresAt: String,
+    val turnDuration: String,
+    val minThings: Int?,
+    val maxThings: Int?,
+    val minPlayers: Int?,
+    val maxPlayers: Int?,
+    val players: List<SafeGamePlayerData>,
     val things: List<SafeGameThingData>,
+)
+
+internal data class SafeGamePlayerData(
+    val id: String,
+    val nodeId: String,
+    val createdAt: String,
+    val firstName: String,
+    val lastName: String,
+    val score: Int,
 )
 
 @Serializable
