73a722fcf9c7a5c4ba719ce5962e1469858c43e3
RJ <rybertjen@gmail.com>
2026-08-14 18:39:17 -0700

    feat(clue): add manual authoring actions

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanEditAction.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanEditAction.kt
@@ -5,6 +5,18 @@ import com.micrantha.eyespie.domain.ai.GeneratedClues
 sealed interface ScanEditAction {
     data class Init(val params: ScanEditParams) : ScanEditAction
 
+    data object GenerateClues : ScanEditAction
+
+    data object GeneratedCluesUnavailable : ScanEditAction
+
+    data object UseManualAuthoring : ScanEditAction
+
+    data object UseGeneratedAuthoring : ScanEditAction
+
+    data class UpdateManualClue(val value: String) : ScanEditAction
+
+    data class UpdateManualAnswer(val value: String) : ScanEditAction
+
     data object SaveScanEdit : ScanEditAction
 
     data object SaveThingError : ScanEditAction
