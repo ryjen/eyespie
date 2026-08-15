2f94d65fe30c1cb44a6e28f810718a6424766e14
RJ <rybertjen@gmail.com>
2026-08-15 02:17:14 -0700

    fix(ios): remove stale backend app delegate bridge

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -1,23 +1,3 @@
 import UIKit
-import eyespie
 
-private let app = eyespie.AppDelegate(
-    networkMonitor: iOSNetworkMonitor(),
-    packageId: "com.micrantha.eyespie"
-)
-
-class iOSAppDelegate: NSObject, UIApplicationDelegate {
-
-    var backgroundSessionCompletionHandler: (() -> Void)?
-
-    func application(_ application: UIApplication,
-                     handleEventsForBackgroundURLSession identifier: String,
-                     completionHandler: @escaping () -> Void) {
-        backgroundSessionCompletionHandler = completionHandler
-     }
-
-    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
-        backgroundSessionCompletionHandler?()
-        backgroundSessionCompletionHandler = nil
-    }
-}
+class iOSAppDelegate: NSObject, UIApplicationDelegate {}
