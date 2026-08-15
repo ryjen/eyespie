edd66d00e09b32d5e62f4d7e0d18bee2c2b694ee
RJ <rybertjen@gmail.com>
2026-08-15 04:51:02 -0700

    feat(game): inject iOS offline runtime

--- eyespie/src/iosMain/kotlin/MainViewController.kt
+++ eyespie/src/iosMain/kotlin/MainViewController.kt
@@ -1,5 +1,9 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import com.micrantha.eyespie.App
+import com.micrantha.eyespie.game.createIosOfflineRuntime
 import platform.UIKit.UIViewController
 
-fun MainViewController(): UIViewController = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController {
+    val runtime = createIosOfflineRuntime()
+    return ComposeUIViewController { App(runtime) }
+}
