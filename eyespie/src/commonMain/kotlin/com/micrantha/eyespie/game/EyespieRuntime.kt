dfb786acab370b560c82c7bea102f24037125a0e
RJ <rybertjen@gmail.com>
2026-08-15 13:23:57 -0700

    feat(sharing): compose bundle service into runtime

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/game/EyespieRuntime.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/game/EyespieRuntime.kt
@@ -1,5 +1,8 @@
 package com.micrantha.eyespie.game
 
+import com.micrantha.eyespie.sharing.GameBundleService
+
 class EyespieRuntime(
     val gameLoop: LocalGameLoop,
+    val bundleService: GameBundleService,
 )
