76d4ac70ca66fa46e0a7c8c17b984122c1c5e7a4
RJ <rybertjen@gmail.com>
2026-08-14 18:39:29 -0700

    feat(clue): expose authoring UI state

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanEditUiState.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/scan/entities/ScanEditUiState.kt
@@ -5,7 +5,12 @@ import androidx.compose.ui.graphics.painter.Painter
 data class ScanEditUiState(
     val clues: Collection<ScanClue>,
     val image: Painter?,
+    val authoringMode: ClueAuthoringMode,
+    val manualClue: String,
+    val manualAnswer: String,
+    val generationUnavailable: Boolean,
+    val canUseGenerated: Boolean,
     val isBusy: Boolean,
     val enabled: Boolean,
-    val isError: Boolean
+    val isError: Boolean,
 )
