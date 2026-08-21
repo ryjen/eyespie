49a204ab3c92f11c55cca85bfd62c9c8e58905b0
RJ <rybertjen@gmail.com>
2026-08-21 16:39:42 -0700

    Add Home route host

---
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/home/HomeRoute.kt
@@ -0,0 +1,24 @@
+package com.micrantha.eyespie.features.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+
+@Composable
+fun HomeRoute(factory: HomeFactory) {
+    val scope = rememberCoroutineScope()
+    val interactor = remember(factory, scope) { factory.create(scope) }
+    val state by interactor.state.collectAsState()
+
+    LaunchedEffect(interactor) {
+        interactor.dispatch(HomeIntent.Refresh)
+    }
+
+    HomeScreen(
+        state = state,
+        dispatch = interactor::dispatch,
+    )
+}
