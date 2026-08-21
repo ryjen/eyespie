17f852a08508ff0892b96e24bbdc4044d10e5b8d
RJ <rybertjen@gmail.com>
2026-08-21 16:40:55 -0700

    Add Create capability port

---
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/create/CreateGamePort.kt
@@ -0,0 +1,14 @@
+package com.micrantha.eyespie.features.create
+
+import com.micrantha.eyespie.game.CreatedGame
+import com.micrantha.eyespie.game.LocalGameResult
+import com.micrantha.eyespie.imaging.CapturedImage
+
+interface CreateGamePort {
+    suspend fun create(
+        name: String,
+        clueText: String,
+        expectedAnswer: String,
+        targetImage: CapturedImage,
+    ): LocalGameResult<CreatedGame>
+}
