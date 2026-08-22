92dab3a518bc5d43bf6c9b13231b3718e188ba18
RJ <rybertjen@gmail.com>
2026-08-22 05:01:45 -0700

    refactor: remove utility port

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/utility/UtilityPort.kt
+++
@@ -1,7 +0,0 @@
-package com.micrantha.eyespie.features.utility
-
-import com.micrantha.eyespie.game.LocalGameResult
-
-interface UtilityPort {
-    suspend fun loadUtility(): LocalGameResult<UtilityContent>
-}
