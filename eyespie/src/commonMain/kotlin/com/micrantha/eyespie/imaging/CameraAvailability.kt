7524ece8878fec9e0e94e9d9700a96acb5198ab4
RJ <rybertjen@gmail.com>
2026-08-22 00:37:57 -0700

    revert: keep camera availability work on feature branch

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/imaging/CameraAvailability.kt
+++
@@ -1,9 +0,0 @@
-package com.micrantha.eyespie.imaging
-
-/** Platform-neutral camera availability exposed to feature presentation. */
-sealed interface CameraAvailability {
-    data object Requestable : CameraAvailability
-    data object Ready : CameraAvailability
-    data object PermissionDenied : CameraAvailability
-    data object Unavailable : CameraAvailability
-}
