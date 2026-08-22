c0b792a0603d17c5d8e5c676063fd0262d4309ca
RJ <rybertjen@gmail.com>
2026-08-21 21:14:05 -0700

    feat: extend authoring port for existing games

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/create/CreateGamePort.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/create/CreateGamePort.kt
@@ -1,5 +1,7 @@
 package com.micrantha.eyespie.features.create
 
+import com.micrantha.eyespie.core.GameId
+import com.micrantha.eyespie.game.CreatedClue
 import com.micrantha.eyespie.game.CreatedGame
 import com.micrantha.eyespie.game.LocalGameResult
 import com.micrantha.eyespie.imaging.CapturedImage
@@ -11,4 +13,11 @@ interface CreateGamePort {
         expectedAnswer: String,
         targetImage: CapturedImage,
     ): LocalGameResult<CreatedGame>
+
+    suspend fun addClue(
+        gameId: GameId,
+        clueText: String,
+        expectedAnswer: String,
+        targetImage: CapturedImage,
+    ): LocalGameResult<CreatedClue>
 }
