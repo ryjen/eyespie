2d5d5887fff4aac56d97d87f8b878c7d37ab4ea7
RJ <rybertjen@gmail.com>
2026-08-21 22:15:15 -0700

    feat: add utility factory to app graph

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/app/AppGraph.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/app/AppGraph.kt
@@ -6,11 +6,13 @@ import com.micrantha.eyespie.features.gamedetail.GameDetailFactory
 import com.micrantha.eyespie.features.home.HomeFactory
 import com.micrantha.eyespie.features.onboarding.OnboardingFactory
 import com.micrantha.eyespie.features.play.PlayGameFactory
+import com.micrantha.eyespie.features.utility.UtilityFactory
 
 class AppGraph(
     val navigator: AppNavigator,
     val homeFactory: HomeFactory,
     val onboardingFactory: OnboardingFactory,
+    val utilityFactory: UtilityFactory,
     val createGameFactory: CreateGameFactory,
     val gameDetailFactory: GameDetailFactory,
     val clueAuthoringFactory: ClueAuthoringFactory,
