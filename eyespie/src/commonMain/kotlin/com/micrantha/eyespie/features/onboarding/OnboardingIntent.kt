03b00e7a423ca89901e640b9ea59d4421d25a62a
RJ <rybertjen@gmail.com>
2026-08-21 18:01:33 -0700

    feat(onboarding): add explicit skip intent

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/onboarding/OnboardingIntent.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/onboarding/OnboardingIntent.kt
@@ -3,6 +3,7 @@ package com.micrantha.eyespie.features.onboarding
 sealed interface OnboardingIntent {
     data object Next : OnboardingIntent
     data object Previous : OnboardingIntent
+    data object Skip : OnboardingIntent
     data object Done : OnboardingIntent
     data object Back : OnboardingIntent
 }
