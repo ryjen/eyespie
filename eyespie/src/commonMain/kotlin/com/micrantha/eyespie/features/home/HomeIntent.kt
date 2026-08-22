5a4b4779691b8c9410c37f8d3fd1126d7a0037a4
RJ <rybertjen@gmail.com>
2026-08-22 02:11:42 -0700

    feat(home): add import preview intents

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/home/HomeIntent.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/home/HomeIntent.kt
@@ -8,11 +8,14 @@ sealed interface HomeIntent {
     data object DismissFailure : HomeIntent
     data object DismissImportResult : HomeIntent
     data object ImportSelected : HomeIntent
+    data object ImportConfirmed : HomeIntent
+    data object ImportPreviewCancelled : HomeIntent
     data object OnboardingSelected : HomeIntent
     data object UtilitySelected : HomeIntent
     data object CreateSelected : HomeIntent
     data class GameSelected(val gameId: GameId) : HomeIntent
     data class ContentLoaded(val generation: Long, val content: HomeContent) : HomeIntent
     data class OperationFailed(val failure: LocalGameFailure, val generation: Long) : HomeIntent
+    data class ImportPreviewReady(val preview: HomeImportPreview) : HomeIntent
     data class ImportFinished(val result: HomeImportResult) : HomeIntent
 }
