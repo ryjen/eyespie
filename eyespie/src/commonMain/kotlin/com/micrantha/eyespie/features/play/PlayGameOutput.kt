1df3b64e5d3dc0a381c441c1d0e572116092db4c
RJ <rybertjen@gmail.com>
2026-08-21 21:33:41 -0700

    feat(play): add semantic next-clue output

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/play/PlayGameOutput.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/play/PlayGameOutput.kt
@@ -1,7 +1,9 @@
 package com.micrantha.eyespie.features.play
 
 import com.micrantha.eyespie.core.GameId
+import com.micrantha.eyespie.core.ThingId
 
 sealed interface PlayGameOutput {
     data class Closed(val gameId: GameId) : PlayGameOutput
+    data class Advance(val gameId: GameId, val thingId: ThingId) : PlayGameOutput
 }
