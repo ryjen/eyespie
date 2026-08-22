d92baa7a8af265f41704816cde7a222119a4cf43
RJ <rybertjen@gmail.com>
2026-08-22 04:55:41 -0700

    refactor: migrate clue authoring interactor to base mechanics

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/clueauthoring/ClueAuthoringInteractor.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/clueauthoring/ClueAuthoringInteractor.kt
@@ -2,11 +2,8 @@ package com.micrantha.eyespie.features.clueauthoring
 
 import com.micrantha.eyespie.core.GameId
 import com.micrantha.eyespie.game.LocalGameResult
-import com.micrantha.eyespie.mvi.Interactor
+import com.micrantha.eyespie.mvi.BaseInteractor
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class ClueAuthoringInteractor(
@@ -15,13 +12,12 @@ class ClueAuthoringInteractor(
     private val gameId: GameId,
     private val output: (ClueAuthoringOutput) -> Unit,
     initialState: ClueAuthoringState = ClueAuthoringState(),
-) : Interactor<ClueAuthoringState, ClueAuthoringIntent> {
-    private val mutableState = MutableStateFlow(initialState)
-    override val state: StateFlow<ClueAuthoringState> = mutableState.asStateFlow()
-
-    override fun dispatch(intent: ClueAuthoringIntent) {
-        val previousState = mutableState.value
-        mutableState.value = ClueAuthoringReducer.reduce(previousState, intent)
+) : BaseInteractor<ClueAuthoringState, ClueAuthoringIntent>(initialState, ClueAuthoringReducer) {
+    override fun afterReduce(
+        intent: ClueAuthoringIntent,
+        previousState: ClueAuthoringState,
+        stateAfterReduce: ClueAuthoringState,
+    ) {
         when (intent) {
             is ClueAuthoringIntent.TargetCaptured -> if (!previousState.busy) {
                 scope.launch {
