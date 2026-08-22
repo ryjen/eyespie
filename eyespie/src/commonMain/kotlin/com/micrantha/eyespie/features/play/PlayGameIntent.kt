6951fc8d435027f9938c177efe10b3c745921335
RJ <rybertjen@gmail.com>
2026-08-21 21:32:27 -0700

    feat: version play effects and add continue intent

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/play/PlayGameIntent.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/play/PlayGameIntent.kt
@@ -6,11 +6,12 @@ import com.micrantha.eyespie.imaging.CapturedImage
 
 sealed interface PlayGameIntent {
     data object Load : PlayGameIntent
-    data class ContentLoaded(val content: PlayGameContent) : PlayGameIntent
-    data class LoadFailed(val failure: LocalGameFailure) : PlayGameIntent
+    data class ContentLoaded(val generation: Long, val content: PlayGameContent) : PlayGameIntent
+    data class LoadFailed(val generation: Long, val failure: LocalGameFailure) : PlayGameIntent
     data class GuessCaptured(val image: CapturedImage) : PlayGameIntent
-    data class GuessCompleted(val outcome: GuessOutcome) : PlayGameIntent
-    data class OperationFailed(val failure: LocalGameFailure) : PlayGameIntent
+    data class GuessCompleted(val generation: Long, val outcome: GuessOutcome) : PlayGameIntent
+    data class OperationFailed(val generation: Long, val failure: LocalGameFailure) : PlayGameIntent
+    data object Continue : PlayGameIntent
     data object CameraFailed : PlayGameIntent
     data object DismissFailure : PlayGameIntent
     data object Back : PlayGameIntent
