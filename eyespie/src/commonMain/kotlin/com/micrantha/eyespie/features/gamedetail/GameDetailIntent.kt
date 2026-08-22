ec9cc88e6364fd4c2201e9be5768ccf3b7b6bf23
RJ <rybertjen@gmail.com>
2026-08-21 18:34:39 -0700

    feat(game-detail): add share intents

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/gamedetail/GameDetailIntent.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/gamedetail/GameDetailIntent.kt
@@ -7,7 +7,10 @@ sealed interface GameDetailIntent {
     data object Load : GameDetailIntent
     data object Back : GameDetailIntent
     data object DismissFailure : GameDetailIntent
+    data object DismissShareResult : GameDetailIntent
+    data object ShareSelected : GameDetailIntent
     data class PlaySelected(val thingId: ThingId) : GameDetailIntent
     data class ContentLoaded(val generation: Long, val content: GameDetailContent) : GameDetailIntent
     data class OperationFailed(val generation: Long, val failure: LocalGameFailure) : GameDetailIntent
+    data class ShareFinished(val result: GameDetailShareResult) : GameDetailIntent
 }
