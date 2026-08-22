d9eb6c641e8b7ca066c6352e13749614af3db66c
RJ <rybertjen@gmail.com>
2026-08-21 21:14:09 -0700

    feat: model new-game versus add-clue authoring

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/create/CreateGameState.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/create/CreateGameState.kt
@@ -1,6 +1,14 @@
 package com.micrantha.eyespie.features.create
 
+import com.micrantha.eyespie.core.GameId
+
+sealed interface CreateGameMode {
+    data object NewGame : CreateGameMode
+    data class AddClue(val gameId: GameId) : CreateGameMode
+}
+
 data class CreateGameState(
+    val mode: CreateGameMode = CreateGameMode.NewGame,
     val name: String = "",
     val clue: String = "",
     val expectedAnswer: String = "",
