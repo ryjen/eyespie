281a108cbb940270cc72d33b850b6539d08210f0
RJ <rybertjen@gmail.com>
2026-08-21 22:12:51 -0700

    feat: add utility port

---
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/utility/UtilityPort.kt
@@ -0,0 +1,7 @@
+package com.micrantha.eyespie.features.utility
+
+import com.micrantha.eyespie.game.LocalGameResult
+
+interface UtilityPort {
+    suspend fun load(): LocalGameResult<UtilityContent>
+}
