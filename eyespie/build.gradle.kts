0e0a75b03d3b554e2a96d42c69781a33c19de100
RJ <rybertjen@gmail.com>
2026-08-23 02:38:16 -0700

    build: enable Voyager navigator

--- eyespie/build.gradle.kts
+++ eyespie/build.gradle.kts
@@ -129,6 +129,7 @@ kotlin {
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
             implementation(libs.compose.components.resources)
+            implementation(libs.voyager.navigator)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.sqldelight.coroutines)
             implementation(libs.okio)
