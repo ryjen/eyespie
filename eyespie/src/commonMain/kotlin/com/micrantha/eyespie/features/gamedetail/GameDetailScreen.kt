4a09bb1e9e3167c428365cbd5f00a9a7a18c1253
RJ <rybertjen@gmail.com>
2026-09-01 21:01:39 -0700

    fix(ui): correct game detail imports

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/gamedetail/GameDetailScreen.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/gamedetail/GameDetailScreen.kt
@@ -10,11 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.rememberScrollState
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
