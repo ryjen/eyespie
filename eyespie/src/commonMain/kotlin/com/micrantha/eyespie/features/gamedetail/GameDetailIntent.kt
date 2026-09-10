50df9016e9659b50136eb6427781a5b73faeee95
RJ <rybertjen@gmail.com>
2026-09-09 18:12:53 -0700

    feat(gamedetail): add explicit save-file intents

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/gamedetail/GameDetailIntent.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/gamedetail/GameDetailIntent.kt
@@ -9,8 +9,10 @@ sealed interface GameDetailIntent {
     data object AddClueSelected : GameDetailIntent
     data object DismissFailure : GameDetailIntent
     data object ShareSelected : GameDetailIntent
+    data object SaveSelected : GameDetailIntent
     data class PlaySelected(val thingId: ThingId) : GameDetailIntent
     data class ContentLoaded(val generation: Long, val content: GameDetailContent) : GameDetailIntent
     data class OperationFailed(val generation: Long, val failure: LocalGameFailure) : GameDetailIntent
     data class ShareFinished(val result: GameDetailShareResult) : GameDetailIntent
+    data class SaveFinished(val result: GameDetailSaveResult) : GameDetailIntent
 }
