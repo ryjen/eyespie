1ddc631ebf51a1961f641fc7d21baa39059b88ed
RJ <rybertjen@gmail.com>
2026-09-09 19:55:21 -0700

    test(android): pin external document Robolectric sdk

--- eyespie/src/androidUnitTest/kotlin/com/micrantha/eyespie/sharing/AndroidExternalGameIntentTest.kt
+++ eyespie/src/androidUnitTest/kotlin/com/micrantha/eyespie/sharing/AndroidExternalGameIntentTest.kt
@@ -8,8 +8,10 @@ import kotlin.test.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class AndroidExternalGameIntentTest {
     @Test
     fun view_with_eyespie_media_type_is_accepted() {
