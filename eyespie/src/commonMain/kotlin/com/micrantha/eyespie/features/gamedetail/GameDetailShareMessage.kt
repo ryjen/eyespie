2343571dc8bbc6d1d0ce05291340ecaebff0ca42
RJ <rybertjen@gmail.com>
2026-09-09 18:03:09 -0700

    fix(sharing): do not claim delivery when chooser opens

--- eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/gamedetail/GameDetailShareMessage.kt
+++ eyespie/src/commonMain/kotlin/com/micrantha/eyespie/features/gamedetail/GameDetailShareMessage.kt
@@ -4,6 +4,7 @@ import com.micrantha.eyespie.generated.resources.*
 import org.jetbrains.compose.resources.StringResource
 
 internal fun gameDetailShareMessageResource(result: GameDetailShareResult): StringResource? = when (result) {
+    GameDetailShareResult.Presented -> null
     GameDetailShareResult.Shared -> Res.string.feedback_game_shared
     GameDetailShareResult.NotLocalCreator -> Res.string.failure_share_not_local_creator
     GameDetailShareResult.TooLarge -> Res.string.failure_share_too_large
