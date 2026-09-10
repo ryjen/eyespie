0f5dd0a8b59ec6e83e779d76c7ac4b0233b4529a
RJ <rybertjen@gmail.com>
2026-09-09 18:01:58 -0700

    feat(android): route external .eyespie opens into app import

--- eyespie/src/androidMain/kotlin/com/micrantha/eyespie/android/MainActivity.kt
+++ eyespie/src/androidMain/kotlin/com/micrantha/eyespie/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.micrantha.eyespie.android
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -9,11 +10,17 @@ import androidx.compose.runtime.remember
 import com.micrantha.eyespie.App
 import com.micrantha.eyespie.AppUnavailable
 import com.micrantha.eyespie.game.createAndroidEyespieRuntime
+import com.micrantha.eyespie.sharing.AndroidGameDocumentTransfer
+import com.micrantha.eyespie.sharing.externalEyespieDocumentUri
 import com.micrantha.eyespie.sharing.rememberAndroidGameDocumentTransfer
 
 class MainActivity : ComponentActivity() {
+    private lateinit var documentTransfer: AndroidGameDocumentTransfer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        documentTransfer = AndroidGameDocumentTransfer(contentResolver)
+        offerExternalGameDocument(intent)
         enableEdgeToEdge()
         setContent {
             val runtime = remember {
@@ -27,9 +34,26 @@ class MainActivity : ComponentActivity() {
             if (runtime == null) {
                 AppUnavailable()
             } else {
-                val documentTransfer = rememberAndroidGameDocumentTransfer()
-                App(runtime, documentTransfer)
+                val transfer = rememberAndroidGameDocumentTransfer(documentTransfer)
+                App(
+                    runtime = runtime,
+                    documentTransfer = transfer,
+                    externalDocumentSource = documentTransfer,
+                )
             }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        offerExternalGameDocument(intent)
+    }
+
+    private fun offerExternalGameDocument(intent: Intent?) {
+        val uri = externalEyespieDocumentUri(intent) ?: return
+        if (!documentTransfer.offerExternalDocument(uri)) {
+            Log.w(TAG, "External Eyespie document ignored while another document operation is active")
         }
     }
 
